8e2d9aa600ee7ebc8be04c9064c12a2cd86ea638
heimoshuiyu <heimoshuiyu@gmail.com>
2020-06-01 13:54:36 +0800

    弃用newDay，使用睡眠24小时

--- Assistant.py
+++ Assistant.py
@@ -7,30 +7,34 @@ from MessageRead import ReadMessage
 
 class Main:
     def __init__(self):
-        self.newDay = 0
 
     def heartBeat(self):
         # 心跳功能，防止程序死的悄无声息
         now = time.localtime(time.time())
-        if os.path.exists("./log/%s.txt" % (str(time.strftime("%Y%m%d", time.localtime())))) or self.newDay == 2:
+        if os.path.exists("./log/%s.txt" % (str(time.strftime("%Y%m%d", time.localtime())))):
             pass
         else:
-            self.newDay = 1
             logger('本次启动时间为: %s' % (str(time.asctime( time.localtime(time.time()) ))),path="./log/")
             # 确保休眠到早上7点后再发心跳
             if (7 - now[3]) >= 0:
                 time.sleep(3600*(7-now[3]-1) + 60*(60-now[4]))
             mail_sender.mail_sender.send("Dear Master, I am still alive! Take it easy.", "This is a Heartbeat function message. I'll send you every day so that you can know I'm there.")
 
+    def headBeatThread(self):
+        while 1:
+            self.heartBeat()
+            time.sleep(60 * 60 * 24)
+
     def main(self):
+        thread = threading.Thread(target=self.heartBeatThread, args=(), daemon=True)
+        thread.start()
+
         while 1:
             # 此处写你的敏感信息
             data["o_usr"]=""
             data["o_pwd"]=""
             data["s_usr"]=""
             data["s_pwd"]=""
-            thread = threading.Thread(target=self.heartBeat, args=(), daemon=True)
-            thread.start()
             try:
                 r = ReadMessage(data)
 
